1f2c8edd365e506c4a5449801705ea1a17f07fc1
gscialchi <gastonscialchi@outlook.com>
2026-03-17 13:33:25 -0300

    Reproduce Fig. 3

--- scripts/reproduce_fig3.py
+++ scripts/reproduce_fig3.py
@@ -1,73 +1,99 @@
-# make copies to avoid conflicts
-doer_evolve_f = doer_evolve_distribution.copy()
-doer_gs = doer_gram_schmidt.copy()
-doer_evolve_rho = doer_evolve_operator.copy()
-doer_arnoldi = doer_arnoldi_operator.copy()
-doer_rho = doer_rho_gauss.copy()
-doer_u = doer_u_ho.copy()
+from functools import partial
 
-# global parameters
-n_steps = 100
-stop = n_steps
-q0, p0 = 1, 0
-s = 0.1 # classical standard deviation
+import numpy as mp
 
-## fix dt such that in one timestep there is some overlap (if a<~1)
+from qtoc_krylov.harmonic import *
+from qtoc_krylov.evolve import *
+from qtoc_krylov.krylov import *
+from qtoc_krylov.misc import *
+from qtoc_krylov.utilities.paths import *
+from qtoc_krylov.utilities.doer import Doer
+
+from plotters import plot_states_correspondence
+
+
+#### Setup Doers for data saving & retrieval
+doer_evolve_f = Doer(evolve_distribution, path=CALC_DIR)
+
+doer_gs = Doer(gram_schmidt_ft, ignore_args='ft', path=CALC_DIR)
+
+doer_arnoldi = Doer(arnoldi_FO_operator, path=CALC_DIR)
+
+doer_rho = Doer(coherent_ensemble, args={'f': gauss_2D})
+
+doer_u = Doer(u_harmonic)
+
+
+#### Calculation parameters
+n_steps = 200 # number of time-steps
+stop = n_steps # when to stop calculating Krylov
+
+q0, p0 = 1, 0 # center of position initial state in phase space
+s = 0.1 # standard deviation for classical distribution
+
+# fix dt such that in one timestep there is some overlap (if a<~1)
 a = 0.5
 dt = a * 2*s/(q0**2 + p0**2)**0.5
-print(f'dt={dt}')
 
-## Classical
-N = 300
+# number of points to evaluate classical distribution
+N_res = 300 # make sure is enough to resolve packet!
 
-r = (q0**2+p0**2)**0.5 + 5*s # 5 sigma space between packet and border
+# define limits of phase space where the distribution is evaluated
+r = (q0**2+p0**2)**0.5 + 5*s # 5 sigma space between packet center and limits
 qlim = [-r, r]
 plim = qlim
 
-f = partial(gauss_2D, x0=q0, y0=p0, s=s)
-map = partial(harmonic_map_inv, dt=dt)
+f = partial(gauss_2D, x0=q0, y0=p0, s=s) # initial classical distribution
+map = partial(harmonic_map_inv, dt=dt) # inverse harmonic map
 
-doer_evolve_f.set_args(map=map,
-                       qlim=qlim, plim=plim, N=N,
-                       rho=f, n_steps=n_steps)
-ft = doer_evolve_f.doit()
-
-doer_gs.set_args(ft=ft, qlim=qlim, plim=plim, N=N, stop=stop)
-doer_gs.set_fakeargs(map=map, rho=f)
-fk = doer_gs.doit()
-
-## Quantum
-#  hs = 1/np.asarray([2**4, 2**5, 2**6, 2**7, 2**8])[:2]
-#  hs = 1/np.asarray([2**4, 2**6, 2**8])
-hs = 1/np.asarray([2**5, 2**8])
+hs = 1/np.asarray([2**5, 2**7]) # values of hbar to evaluate at
 fid = 1-1e-9 # fidelity of truncated quantum coherent state
 
-doer_rho.set_args(args=(q0, p0, s))
+# integration limits that will be used to calculate the quantum initial state
+nsig = 5 # n sigma padding for integration lims & cutoffs
+q_integlims = (q0 - nsig*s, q0 + nsig*s)
+p_integlims = (p0 - nsig*s, p0 + nsig*s)
 
-krys = []
-cutoffs_list = []
+doer_rho.set_args(args=(q0, p0, s)) # set parameters for initial quantum distribution
+
+
+#### Classical
+doer_evolve_f.set_args(map=map,
+                       qlim=qlim, plim=plim, N=N_res,
+                       rho=f, n_steps=n_steps)
+evo_cl = doer_evolve_f.doit() # get evolution
+
+doer_gs.set_args(ft=evo_cl, qlim=qlim, plim=plim, N=N_res, stop=stop)
+doer_gs.set_fakeargs(map=map, rho=f)
+kry_cl = doer_gs.doit() # get Krylov via Gram-Schmidt
+
+
+#### Quantum
+krys_qu = [] # Krylov states for each hbar
+cutoffs_list = [] # keep track of cutoffs used for each hbar
 for i, h in enumerate(hs):
-    nsig = 5 # n sigma padding for integ lims & cutoffs
-    qlims = (q0 - nsig*s, q0 + nsig*s)
-    plims = (p0 - nsig*s, p0 + nsig*s)
-    cutoffs = cutoffs_for_coherent_ensemble(fid, qlims, plims, hbar=h)
+    # find where to cutoff energy levels for the given fidelity, hbar and lims
+    cutoffs = cutoffs_for_coherent_ensemble(fid, q_integlims, p_integlims,
+                                            hbar=h)
 
-    doer_rho.set_args(qlims=qlims, plims=plims, cutoffs=cutoffs, hbar=h)
+    # Apply parameters for given value of hbar
+    doer_rho.set_args(qlims=q_integlims, plims=p_integlims, cutoffs=cutoffs,
+                      hbar=h)
+
     doer_u.set_args(dt=dt, cutoffs=cutoffs, hbar=h)
-
-    doer_evolve_rho.set_args(u=doer_u, rho=doer_rho,
-                             n_steps=n_steps)
-    rho_evo = doer_evolve_rho.doit()
 
     doer_arnoldi.set_args(U=doer_u, e0=doer_rho,
                           stop=stop,
                           prod=partial(operator_prod, hbar=h))
-    rho_kry = doer_arnoldi.doit()
 
-    krys.append(rho_kry)
+    # Calculate quantum
+    kry_qu = doer_arnoldi.doit() # quantum Krylov basis via Arnoldi
+
+    krys_qu.append(kry_qu)
     cutoffs_list.append(cutoffs)
 
-### compare
+
+#### Plot
 which = [0, 1, 2, 3, 15, 35, 55] # which states to show
 
 # points for drawing classical trayectories
@@ -75,12 +101,14 @@ point_map = partial(harmonic_map, dt=dt)
 qq0 = 2*np.linspace(*qlim, 2*15); pp0 = np.zeros_like(qq0)
 points = [qq0, pp0]
 
-figname = f'manuscript/HO_states_correspondence'
-plot_states_correspondence(fk, krys, qlim, plim, N, hbars=hs,
+figname = 'Figure_3'
+plot_states_correspondence(kry_cl, krys_qu,
+                           qlim, plim, N_res,
+                           hbars=hs,
                            points=points, map=point_map,
                            norm_from_k0=True,
                            which=which, cutoffs=cutoffs_list,
-                           #  save=True,
+                           save=True,
                            #  show=False,
                            savedir=FIG_DIR + figname,
-                           saveformat='png')
+                           saveformat='png') # pdf can be large
