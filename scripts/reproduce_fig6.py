eb0f036b0a5f253008b1a3aa8eeb877e1b9c28c0
gscialchi <gastonscialchi@outlook.com>
2026-03-17 14:50:39 -0300

    Reproduce Fig. 6

--- scripts/reproduce_fig6.py
+++ scripts/reproduce_fig6.py
@@ -1,60 +1,82 @@
-# make copies to avoid conflicts
-doer_evolve_f = doer_evolve_distribution.copy()
-doer_gs = doer_gram_schmidt.copy()
-doer_evolve_rho = doer_evolve_operator.copy()
-doer_arnoldi = doer_arnoldi_operator.copy()
-doer_rho = doer_rho_gauss_torus.copy()
-doer_u = doer_u_harper.copy()
+from functools import partial
 
-# global parameters
-n_steps = 100
-stop = n_steps
-k = 0.05
-q0, p0 = 0.4, 0.5
-s = 0.025 # classical standard deviation
+import numpy as mp
 
-## Classical
-N_cl = 300
-qlim, plim = [0, 1], [0, 1]
+from qtoc_krylov.harper import *
+from qtoc_krylov.evolve import *
+from qtoc_krylov.krylov import *
+from qtoc_krylov.misc import *
+from qtoc_krylov.utilities.paths import *
+from qtoc_krylov.utilities.doer import Doer
 
-f = partial(periodic_gauss_2D, x0=q0, y0=p0, s=s)
-map = partial(c_harper_inv, k)
+from plotters import plot_states_correspondence
 
+
+#### Setup Doers for data saving & retrieval
+doer_evolve_f = Doer(evolve_distribution, path=CALC_DIR)
+
+doer_gs = Doer(gram_schmidt_ft, ignore_args='ft', path=CALC_DIR)
+
+doer_arnoldi = Doer(arnoldi_FO_operator, path=CALC_DIR)
+
+doer_rho = Doer(coherent_ensemble_torus, args={'f': gauss_2D})
+
+doer_u = Doer(q_harper)
+
+
+#### Calculation parameters
+n_steps = 60 # number of time-steps
+stop = n_steps # when to stop calculating Krylov
+
+k = 0.05 # map parameter
+q0, p0 = 0.4, 0.5 # center of initial state in phase space
+s = 0.025 # standard deviation for classical distribution
+
+# number of points to evaluate classical distribution
+N_res = 300 # make sure is enough to resolve packet!
+
+# define limits of phase space where the distribution is evaluated
+qlim, plim = [0, 1], [0, 1] # the whole unit torus
+
+f = partial(periodic_gauss_2D, x0=q0, y0=p0, s=s) # initial classical distribution
+map = partial(c_harper_inv, k) # inverse harper map
+
+N_qus = np.asarray([2**6, 2**11]) # values of quantum dimension
+
+doer_rho.set_args(args=(q0, p0, s)) # parameters for initial quantum distribution
+
+
+#### Classical
 doer_evolve_f.set_args(map=map,
-                       qlim=qlim, plim=plim, N=N_cl,
+                       qlim=qlim, plim=plim, N=N_res,
                        rho=f, n_steps=n_steps)
-ft = doer_evolve_f.doit()
+evo_cl = doer_evolve_f.doit() # get evolution
 
-doer_gs.set_args(ft=ft, qlim=qlim, plim=plim, N=N_cl, stop=stop)
+doer_gs.set_args(ft=evo_cl, qlim=qlim, plim=plim, N=N_res, stop=stop)
 doer_gs.set_fakeargs(map=map, rho=f)
-fk = doer_gs.doit()
+kry_cl = doer_gs.doit() # get Krylov via Gram-Schmidt
 
-## Quantum
-#  hs = 1/np.asarray([2**8, 2**13])
-Ns = np.asarray([2**6, 2**11])
 
-krys = []
-#  for i, h in enumerate(hs):
-for i, N_qu in enumerate(Ns):
-    #  N_qu = int(1/(2*np.pi*h)) + 1
-    print(f'N_qu={N_qu}')
+#### Quantum
+krys_qu = [] # Krylov states for each N_qu
+for i, N_qu in enumerate(N_qus):
+    h = 1/(2*np.pi*N_qu) # hbar correspoding to N_qu
 
-    h = 1/(2*np.pi*N_qu)
-    print(f'h real={h}')
+    # Apply parameters for given value of N_qu
+    doer_rho.set_args(N=N_qu)
 
     doer_u.set_args(k=k, N=N_qu)
-    doer_rho.set_args(N=N_qu, args=(q0, p0, s))
-
-    doer_evolve_rho.set_args(u=doer_u, rho=doer_rho, n_steps=n_steps)
-    rho_qu = doer_evolve_rho.doit()
 
     doer_arnoldi.set_args(U=doer_u, e0=doer_rho, stop=stop,
                           prod=partial(operator_prod, hbar=h))
-    kry_qu = doer_arnoldi.doit()
 
-    krys.append(kry_qu)
+    # Calculate quantum
+    kry_qu = doer_arnoldi.doit() # quantum Krylov basis via Arnoldi
 
-### compare
+    krys_qu.append(kry_qu)
+
+
+#### Plot
 which = [0, 1, 2, 3, 15, 35, 55] # which states to show
 
 # points for drawing classical trayectories
@@ -64,12 +86,15 @@ qq0 = np.append(qq0, [0.05/3]); pp0 = np.append(pp0, [0.5])
 # ^ an extra point at the hyperbolic point
 points = [qq0, pp0]
 
-figname = f'manuscript/Harper_states_correspondence'
-plot_states_correspondence(fk, krys, qlim, plim, N_cl, Nqus=Ns,
+figname = 'Figure_6'
+# calculation of Husimi distributions is inside plotter
+plot_states_correspondence(kry_cl, krys_qu,
+                           qlim, plim, N_res,
+                           Nqus=N_qus,
                            points=points, map=point_map,
                            norm_from_k0=True,
                            which=which,
-                           #  save=True,
+                           save=True,
                            #  show=False,
                            savedir=FIG_DIR + figname,
-                           saveformat='png')
+                           saveformat='png') # pdf can be large
